9e6bbe6fe215b8b9cf51ef44625f1ccafa1be1d5
clarolixe <clarolixe@list.ru>
2024-01-17 15:18:05 +0300

    Initial commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import SearchOrder from '../features/order/SearchOrder.jsx'
+import Username from '../features/user/Username.jsx'
+
+function Header() {
+  return (
+    <header
+      className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200 sm:px-6
+    flex items-center justify-between"
+    >
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
+    </header>
+  )
+}
+
+export default Header
